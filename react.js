b3a66379692d758c58186905d147ba8df8d858c3
sinh nguyen <sinhnd2000@gmail.com>
2022-02-08 15:02:11 +0700

    test xem ok chua

---
+++ react.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class react extends Component {
+    render() {
+        return (
+            <div>
+                <button>click</button>
+            </div>
+        );
+    }
+}
+
+export default react;
